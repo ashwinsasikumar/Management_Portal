5aacd3689c1256257ceb3ea5f2486cd0cf712fb1
AshwinSasikumar <ashwinsasikumar@Ashwins-MacBook-Pro.local>
2026-01-07 14:36:16 +0530

    deploy works

--- .history/client/src/config_20260107141159.js
+++ .history/client/src/config_20260107141159.js
@@ -2,4 +2,5 @@
 // For development: use localhost
 // For production/Docker: use window.location.hostname to get the current host
 const API_BASE_URL = process.env.REACT_APP_API_URL 
+
 export { API_BASE_URL };
